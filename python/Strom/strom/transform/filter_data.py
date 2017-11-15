fixed boundary issues on windowed average and added as a filter [Deliver #152908495]

python/Strom/strom/transform/filter_data.py
@@ -5,6 +5,16 @@ from scipy.signal import butter, filtfilt
 from .transform import Transformer
 
 
+def window_data(in_array, window_len):
+    w_data = np.convolve(in_array, np.ones(window_len), "valid") / window_len
+    # Dealing with the special case for endpoints of in_array
+    ends_divisor = np.arange(1, window_len, 2)
+    start = np.cumsum(in_array[:window_len - 1])[::2] / ends_divisor
+    stop = (np.cumsum(in_array[:-window_len:-1])[::2] / ends_divisor)[::-1]
+    if in_array.shape[0] - w_data.shape[0] - start.shape[0] < stop.shape[0]:
+        stop = stop[1:]
+    return np.concatenate((start, w_data, stop))
+
 class Filter(Transformer):
     __metaclass__ = ABCMeta
 
@@ -24,7 +34,7 @@ class Filter(Transformer):
         """Method to apply the transformation and return the transformed data"""
         raise NotImplementedError("subclass must implement this abstract method.")
 
-class butter_lowpass(Filter):
+class ButterLowpass(Filter):
     """Class to apply a Butterworth lowpass filter to data"""
     def __init__(self):
         super().__init__()
@@ -37,3 +47,18 @@ class butter_lowpass(Filter):
             buttered_data[key] = filtfilt(b, a, np.array(self.data[key]["val"]))
 
         return buttered_data
+
+class WindowAverage(Filter):
+    """Class for windowed average to smooth data"""
+    def __init__(self):
+        super().__init__()
+        self.params["func_params"] = {"window_len": 2}
+
+    def transform_data(self):
+        windowed_data = {}
+        for key in self.data.keys():
+            windowed_data[key] = window_data(np.array(self.data[key]["val"]),
+                                                  self.params["func_params"]["window_len"])
+
+
+        return windowed_data

python/Strom/strom/transform/test/test_filter_data.py
@@ -1,5 +1,6 @@
 import unittest
-from Strom.strom.transform.filter_data import Filter, butter_lowpass
+import numpy as np
+from Strom.strom.transform.filter_data import Filter, ButterLowpass, WindowAverage
 
 
 class TestFilter(unittest.TestCase):
@@ -18,7 +19,7 @@ class TestFilter(unittest.TestCase):
 
 class TestButter(unittest.TestCase):
     def setUp(self):
-        self.butter = butter_lowpass()
+        self.butter = ButterLowpass()
 
     def test_defaults(self):
         self.assertEqual(self.butter.get_params(), {"order":3, "nyquist":0.05})
@@ -30,6 +31,24 @@ class TestButter(unittest.TestCase):
         self.assertIsInstance(buttered_data, dict)
         self.assertIn("viscosity", buttered_data)
         self.assertEqual(len(measure["viscosity"]["val"]), buttered_data["viscosity"].shape[0])
+
+class TestWindow(unittest.TestCase):
+    def setUp(self):
+        self.wa = WindowAverage()
+
+    def test_window(self):
+        test_data_len = 200
+        test_data = np.random.randint(0,15,(test_data_len,))
+        test_measure = {"viscosity":{"val":test_data, "dtype":"int"}}
+        self.wa.load_measures(test_measure)
+        for test_window in range(1, int(np.floor(test_data_len/2))):
+            params = {"func_params":{"window_len":test_window}}
+            self.wa.load_params(params)
+            windowed_data = self.wa.transform_data()
+            self.assertIsInstance(windowed_data, dict)
+            self.assertIn("viscosity", windowed_data)
+            self.assertEqual(len(test_measure["viscosity"]["val"]), windowed_data["viscosity"].shape[0], "window len "+str(test_window))
+
 
 if __name__ == "__main__":
     unittest.main()