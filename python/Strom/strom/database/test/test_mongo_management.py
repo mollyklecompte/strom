011221da19574876aacf780923e1e6f5cd966af8
Molly LeCompte <molly@tura.io>
2017-11-14 17:00:37 -0800

    add second test

--- python/Strom/strom/database/test/test_mongo_management.py
+++ python/Strom/strom/database/test/test_mongo_management.py
@@ -16,9 +16,10 @@ class TestMongoManager(unittest.TestCase):
 
         self.assertEqual(inserted_id, queried['stream_token'])
 
-    #def test_get_template(self):
-     #   queried = self.manager._get_template(self.dstream['stream-token'])
+    def test_get_template(self):
+        queried = self.manager._get_template(self.dstream['stream-token'])
 
+        self.assertEqual(queried['stream_token'], self.dstream['stream_token'])
 
 
 if __name__ == "__main__":
