eda8843223aa4e68ae6890e770cf828cb8757249
tura-david <32076117+tura-david@users.noreply.github.com>
2017-11-16 14:25:12 -0800

    Added func_params and renamed func_param_name to filter_name

--- python/Strom/strom/dstream/filter.py
+++ python/Strom/strom/dstream/filter.py
@@ -6,14 +6,18 @@ __author__ = "Justine <justine@tura.io>"
 
 class Filter(dict):
     def __init__(self):
-        self["func_param_name"] = ""
+        self["filter_name"] = ""
         self["dtype"] = "float(10, 2)"
+        self["func_params"] = {}
 
     def _set_dtype(self, dtype):
         self["dtype"] = dtype
 
-    def _set_func_param_name(self, func_param_name):
-        self["func_param_name"] = func_param_name
+    def _set_filter_name(self, func_param_name):
+        self["filter_name"] = func_param_name
+        
+    def _set_func_params(self, func_params):
+        self["func_params"] = func_params
 
 # def main():
 #     filter_obj = Filter()
