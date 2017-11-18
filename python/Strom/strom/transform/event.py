0cebd8bcc6d4167c7aff51ad4ed489a760f03fd3
tura-david <david@tura.io>
2017-11-17 16:31:23 -0800

    Added tests for basic Event class [deliver #152824145]

--- python/Strom/strom/transform/event.py
+++ python/Strom/strom/transform/event.py
@@ -6,12 +6,16 @@ class Event(dict):
     def __init__(self,  *args, **kwargs):
         self.update(*args, **kwargs)
         expected_keys = ["event_name", "event_rules", "timestamp", "stream_token", "event_context"]
+        bad_keys = []
         for key in self.keys():
             if not key in expected_keys:
-                del self[key]
+                bad_keys.append(key)
+        for key in bad_keys:
+            del self[key]
+
         if not "event_name" in self.keys():
             self["event_name"] = ""
-        if not "event_rules" in self.key():
+        if not "event_rules" in self.keys():
             self["event_rules"] = {}
         if not "timestamp" in self.keys():
             self["timestamp"] = 0
