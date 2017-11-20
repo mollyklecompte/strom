7647ee2dd96835aa7ecaf8a8e226e945baea67e4
tura-david <david@tura.io>
2017-11-20 14:20:41 -0800

    Thresholding event take one [#152897824}

--- python/Strom/strom/transform/detect_event.py
+++ python/Strom/strom/transform/detect_event.py
@@ -1,6 +1,8 @@
 import numpy as np
+from copy import deepcopy
 from abc import ABCMeta, abstractmethod
 from .transform import Transformer
+from .event import Event
 
 class DetectEvent(Transformer):
     __metaclass__ = ABCMeta
@@ -9,11 +11,15 @@ class DetectEvent(Transformer):
         super().__init__()
 
     def load_params(self, params):
-        self.params["event_params"] = params["event_params"]
+        self.params["event_rules"] = params["event_rules"]
         self.params["event_name"] = params["event_name"]
+        self.params["stream_token"] = params["stream_token"]
 
     def get_params(self):
         return self.params
+
+    def add_timestamp(self, timestamp_list):
+        self.data["timestamp"] = {"val":timestamp_list, "dypte":"decimal"}
 
     @abstractmethod
     def transform_data(self):
@@ -23,11 +29,31 @@ class DetectEvent(Transformer):
 class DetectThreshold(DetectEvent):
     def __init__(self):
         super().__init__()
-        self.params["event_params"] = {"measure":"measure_name", "threshold_value":"value to compare against",
+        self.params["event_rules"] = {"measure":"measure_name", "threshold_value":"value to compare against",
                                        "comparison_operator":["==", "!=", ">=", "<=", ">", "<"]}
 
-        @staticmethod
-        def compare_threshold(data_array, comparison_operator, comparision_val):
-            comparisons= {"==":np.equal, "!=":np.not_equal, ">=":np.greater_equal, "<=":np.less_equal, ">":np.greater, "<":np.less}
-            cur_comp = comparisons[comparison_operator]
-            cur_comp(data_array, comparision_val)
+    @staticmethod
+    def compare_threshold(data_array, comparison_operator, comparision_val):
+        comparisons= {"==":np.equal, "!=":np.not_equal, ">=":np.greater_equal, "<=":np.less_equal, ">":np.greater, "<":np.less}
+        cur_comp = comparisons[comparison_operator]
+        match_inds = np.nonzero(cur_comp(data_array, comparision_val))
+        return match_inds[0]
+
+    def create_events(self, event_inds):
+        event_list = []
+        for e_ind in event_inds:
+            cur_event = Event()
+            cur_event["event_name"] = self.params["event_name"]
+            cur_event["event_rules"] = self.params["event_rules"]
+            cur_event["stream_token"] = self.params["stream_token"]
+            cur_event["timestamp"] = self.data["timestamp"]["val"][e_ind]
+            for key, val in self.data:
+                if key != "timestamp":
+                    cur_event["event_context"][key] = val["val"][e_ind]
+            event_list.append(deepcopy(cur_event))
+
+
+    def transform_data(self):
+        measure_array = np.array(self.data[self.params["event_rules"]["measure"]]["val"], dtype=float)
+        event_inds = self.compare_threshold(measure_array, self.params["event_rules"]["comparison_operator"], self.params["event_rules"]["threshold_value"])
+        return self.create_events(event_inds)
