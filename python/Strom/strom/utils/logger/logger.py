08d5a7d74baaa8c4555c3c5c872cf56a001700ee
tura-david <david@tura.io>
2017-11-28 15:07:18 -0800

    Fixed local import

--- python/Strom/strom/utils/logger/logger.py
+++ python/Strom/strom/utils/logger/logger.py
@@ -1,7 +1,7 @@
 import logging
 import logging.config
 import json
-
-config_dict = json.load(open("logger_config.json"))
+import os
+config_dict = json.load(open(os.path.dirname(__file__)+"/logger_config.json"))
 logging.config.dictConfig(config_dict)
 logger = logging.getLogger("stromLogger")
