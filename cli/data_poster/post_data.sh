436caea240786a671975cb744b0603425027fa3a
tura-david <david@tura.io>
2017-12-05 13:16:00 -0800

    using stream_token from defined template [#153391876]

--- cli/data_poster/post_data.sh
+++ cli/data_poster/post_data.sh
@@ -2,14 +2,16 @@
 ### Script to continuously push data to the Flask server ###
 template_data=$(sed "s/\"/\\\\\"/g" ../../python/Strom/demo_data/demo_template.txt | awk '{printf"\"%s\"",$0}')
 echo '{"template":'$template_data'}'  > template.tmp
-curl -H "Content-Type: application/json" -X POST -d @template.tmp 127.0.0.1:5000/api/define
+curl -H "Content-Type: application/json" -X POST -d @template.tmp 127.0.0.1:5000/api/define > stream_token.tmp
 
+st=$(cat stream_token.tmp)
+echo $st
 i="1"
 while [ $i -lt 225 ]
 do
 echo $i
 endi=$[$i+50]
-poster_dstream=$(sed "s/\[{/{/g" ../../python/Strom/demo_data/demo_trip26.txt | sed "s/}\]/}/g" | sed "s/{\"stream_name\"\:/\n&/g" | sed -n "$i","$endi"p | tr "\n" " " | sed "s/\"/\'/g" | awk '{printf"\"[%s]\"",$0}')
+poster_dstream=$(sed "s/\[{/{/g" ../../python/Strom/demo_data/demo_trip26.txt | sed "s/}\]/}/g" | sed "s/{\"stream_name\"\:/\n&/g" | sed -n "$i","$endi"p | tr "\n" " " | sed "s/\"/\'/g" |sed "s/'stream_token': 'abc123'/'stream_token': '$st'/g" | awk '{printf"\"[%s]\"",$0}')
 echo '{"stream_data":'$poster_dstream'}' > chunk.tmp
 curl -H "Content-Type: application/json" -X POST -d @chunk.tmp 127.0.0.1:5000/kafka/load
 i=$(($[$i+50] % 250))
