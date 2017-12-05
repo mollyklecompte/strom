4f77efcc2e0a5545cfa5ae237336e7ee1e72fec2
tura-david <david@tura.io>
2017-12-05 13:38:24 -0800

    Waiting for mariadb to start and running post script from correct directory [#153391876]

--- start_services.sh
+++ start_services.sh
@@ -2,13 +2,14 @@
 echo "Let's get things going"
 ### startup docker containers detached ###
 docker-compose -f docker/support_containers/docker-compose.yml up -d
-sleep 4
+sleep 10 # wait for mariadb to start up
 ### starup flask server in background ###
 cd python/Strom
-python -m strom.strom-api.api.server 2>&1 > /dev/null & echo $!
+python -m strom.strom-api.api.server & echo $!
 cd ../..
-sleep 4
+sleep 2
 ### startup engine in background ###
 echo "All aboard the Engine class!"
 ### start data pushing script ###
-cli/data_poster/post_data.sh
+cd cli/data_poster
+./post_data.sh
