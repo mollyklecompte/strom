12e0d8ab6b15f3df428cdc84b2d46d042862ed3e
Jessica Sheridan <jessicakenney@yahoo.com>
2017-12-06 15:57:21 -0800

    stopwatch for storage threads

--- python/Strom/strom/storage_thread/storage_thread.py
+++ python/Strom/strom/storage_thread/storage_thread.py
@@ -4,6 +4,7 @@ StorageThread class
 """
 from strom.database.mongo_management import MongoManager
 from strom.database.maria_management import SQL_Connection
+from strom.utils.stopwatch import *
 import threading
 
 __version__ = "0.1"
@@ -16,9 +17,11 @@ class StorageRawThread(threading.Thread):
         self._bstream = bstream
         self.maria = SQL_Connection()
         self.rows_inserted = None
+        stopwatch['raw_thread'].start()
 
     def run(self):
         self.rows_inserted = self.maria._insert_rows_into_stream_lookup_table(self._bstream)
+        stopwatch['raw_thread'].lap()
 
 class StorageFilteredThread(threading.Thread):
     def __init__(self, bstream):
@@ -26,11 +29,13 @@ class StorageFilteredThread(threading.Thread):
         self._bstream = bstream
         self.maria = SQL_Connection()
         self.rows_inserted = None
+        stopwatch['filtered_thread'].start()
 
     def run(self):
         filtered_dict = {"stream_token": self._bstream["stream_token"], "timestamp": self._bstream["timestamp"],
                          "filter_measures": self._bstream["filter_measures"]}
         self.rows_inserted = self.maria._insert_rows_into_stream_filtered_table(filtered_dict)
+        stopwatch['filtered_thread'].lap()
 
 class StorageJsonThread(threading.Thread):
     def __init__(self, data, data_type):
@@ -39,6 +44,8 @@ class StorageJsonThread(threading.Thread):
         self._data_type = data_type
         self.mongo = MongoManager()
         self.insert_id = None
+        stopwatch['mongo_thread'].start()
 
     def run(self):
         self.insert_id = self.mongo.insert(self._data, self._data_type)
+        stopwatch['mongo_thread'].lap()
