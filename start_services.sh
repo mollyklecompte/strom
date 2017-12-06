30173fa1e2dc29b065a716e547ed5f1fe5db8430
tura-david <david@tura.io>
2017-12-05 16:21:47 -0800

    starting Flask in new terminal so it is easier to exit cleanly. [#153391876]

--- start_services.sh
+++ start_services.sh
@@ -3,9 +3,10 @@ echo "Let's get things going"
 ### startup docker containers detached ###
 docker-compose -f docker/support_containers/docker-compose.yml up -d
 sleep 10 # wait for mariadb to start up
-### starup flask server in background ###
+### starup flask server in new terminal with same python virtualenv ###
+venv_bin=$(echo $PATH | awk -F: '{print $1}') #you must run this script in a python virtualenv. That will set the first entry in your path to its bin
 cd python/Strom
-python -m strom.strom-api.api.server & echo $!
+gnome-terminal --tab -e "bash --rcfile $venv_bin/activate -ci 'python -m strom.strom-api.api.server'"
 cd ../..
 sleep 2
 ### startup engine in background ###
