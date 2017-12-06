c918f527f17d63c338342bba7b15f8bbd8dae451
tura-david <david@tura.io>
2017-12-06 13:09:20 -0800

    Fixed formatting of input JSON for engine

--- cli/data_poster/post_data.sh
+++ cli/data_poster/post_data.sh
@@ -11,7 +11,8 @@ while [ $i -lt 225 ]
 do
 echo $i
 endi=$[$i+50]
-poster_dstream=$(sed "s/\[{/{/g" ../../python/Strom/demo_data/demo_trip26.txt | sed "s/}\]/}/g" | sed "s/{\"stream_name\"\:/\n&/g" | sed -n "$i","$endi"p | tr "\n" " " | sed "s/\"/\'/g" |sed "s/'stream_token': 'abc123'/'stream_token': '$st'/g" | awk '{printf"\"[%s]\"",$0}')
+
+poster_dstream=$(cat ../../python/Strom/demo_data/demo_trip26.txt | cut -c 2-  | rev | cut -c 2-  | rev | sed "s/{\"stream_name\"\:/\n&/g" | sed -n "$i","$endi"p | tr "\n" " " | sed "s/,\s\+$//" | sed "s/\"/'/g" |sed "s/'stream_token': 'abc123'/'stream_token': '$st'/g" | sed "s/'/\\\\\"/g" | awk '{printf"\"[%s]\"",$0}')
 echo '{"stream_data":'$poster_dstream'}' > chunk.tmp
 curl -H "Content-Type: application/json" -X POST -d @chunk.tmp 127.0.0.1:5000/kafka/load
 i=$(($[$i+50] % 250))
--- start_services.sh
+++ start_services.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 echo "Let's get things going"
 ### startup docker containers detached ###
-docker-compose -f docker/support_containers/docker-compose.yml up -d
+cd  docker/support_containers/
+docker-compose stop && docker-compose rm -f
+docker-compose up -d
 sleep 10 # wait for mariadb to start up
+
 ### starup flask server in new terminal with same python virtualenv ###
 venv_bin=$(echo $PATH | awk -F: '{print $1}') #you must run this script in a python virtualenv. That will set the first entry in your path to its bin
-cd python/Strom
-gnome-terminal --tab -e "bash --rcfile $venv_bin/activate -ci 'python -m strom.strom-api.api.server'"
-cd ../..
+cd ../../python/Strom
+gnome-terminal --tab -e "bash --rcfile $venv_bin/activate -ci 'python -m strom.strom-api.api.server ; bash'"
 sleep 2
+
 ### startup engine in background ###
-echo "All aboard the Engine class!"
+gnome-terminal --tab -e "bash --rcfile $venv_bin/activate -ci 'python -m strom.engine.engine ; bash'"
+
 ### start data pushing script ###
-cd cli/data_poster
+cd ../../cli/data_poster
 ./post_data.sh
