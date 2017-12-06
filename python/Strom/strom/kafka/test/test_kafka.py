db565740272ad6916a7d8cad1613e7aa818bd0c2
Adrian Agnic <adrian@tura.io>
2017-12-05 16:58:06 -0800

    kafka unit tests: init and pipeline: passing

--- python/Strom/strom/kafka/test/test_kafka.py
+++ python/Strom/strom/kafka/test/test_kafka.py
@@ -7,16 +7,17 @@ class TestKafka(unittest.TestCase):
         self.producer = Producer('127.0.0.1:9092', b'test')
         self.consumer = Consumer('127.0.0.1:9092', b'test', 10000)
 
-    def test_kafka(self):
+    def test_kafka_init(self):
+        self.assertIsInstance(self.producer, Producer)
+        self.assertIsInstance(self.consumer, Consumer)
+
+    def test_kafka_pipeline(self):
         message = "Test Message !@%&!"
         message_encode = message.encode()
         self.producer.produce(message_encode)
         consumed_message = self.consumer.engorge()
         consumed_message_decode = consumed_message.decode("utf-8")
         self.assertEqual(consumed_message_decode, "Test Message !@%&!")
-
-    def tearDown(self):
-        pass
 
 if __name__ == '__main__':
     unittest.main()
