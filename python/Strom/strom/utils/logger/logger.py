0279a6f038b623b12ebf23b9cec323caa5716eeb
tura-david <david@tura.io>
2017-12-12 12:27:58 -0800

    Added windowed sum fuction and building tests

--- python/Strom/strom/utils/logger/logger.py
+++ python/Strom/strom/utils/logger/logger.py
@@ -3,5 +3,5 @@ import logging.config
 import json
 import os
 config_dict = json.load(open(os.path.dirname(__file__)+"/logger_config.json"))
-logging.config.dictConfig(config_dict)
 logger = logging.getLogger("stromLogger")
+logging.config.dictConfig(config_dict)
