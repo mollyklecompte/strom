fa2b02ec0012f9e90da8955db1bdeb9570695d85
tura-david <david@tura.io>
2017-12-14 14:06:42 -0800

    Updating to include kafka topic in POST

--- cli/data_poster/post_data.sh
+++ cli/data_poster/post_data.sh
@@ -4,7 +4,7 @@ rm -f template.tmp stream_token.tmp
 template_data=$(sed "s/\"/\\\\\"/g" ../../python/Strom/demo_data/demo_template.txt | awk '{printf"\"%s\"",$0}')
 echo '{"template":'$template_data'}'  > template.tmp
 curl -H "Content-Type: application/json" -X POST -d @template.tmp 127.0.0.1:5000/api/define > stream_token.tmp
-
+kafka_topic=$(awk -F"engine_rules" '{print $2}' ../../python/Strom/demo_data/demo_template.txt | awk -F"kafka" '{print $2}' | sed "s/[^a-zA-Z0-9]//g" | awk '{printf"\"%s\"",$0}')
 st=$(cat stream_token.tmp)
 echo $st
 i="1"
@@ -14,7 +14,7 @@ echo $i
 endi=$[$i+50]
 
 poster_dstream=$(cat ../../python/Strom/demo_data/demo_trip26.txt | cut -c 2-  | rev | cut -c 2-  | rev | sed "s/{\"stream_name\"\:/\n&/g" | sed -n "$i","$endi"p | tr "\n" " " | sed "s/,\s\+$//" | sed "s/\"/'/g" |sed "s/'stream_token': 'abc123'/'stream_token': '$st'/g" | sed "s/'/\\\\\"/g" | awk '{printf"\"[%s]\"",$0}')
-echo '{"stream_data":'$poster_dstream'}' > chunk.tmp
+echo '{"stream_data":'$poster_dstream', "topic":'$kafka_topic'}' > chunk.tmp
 curl -H "Content-Type: application/json" -X POST -d @chunk.tmp 127.0.0.1:5000/kafka/load
 i=$(($[$i+50] % 250))
 echo "take 5"
