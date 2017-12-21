bd0c5d3e7301bbab58e872f02e2c9c188655dbb6
molly lecompte <molly@tura.io>
2017-12-21 12:18:45 -0800

    GOT THE PROCESSING TO WORK FASTLYgit status!

--- python/Strom/strom/engine/engine.py
+++ python/Strom/strom/engine/engine.py
@@ -1,33 +1,27 @@
 """
 Engine Module
-
 Contains...
-
 - class ProcessBstreamThread:
 based off Thread from python standard threading lib,
 takes a bstream dict and a coordinator insatance,
 run method overrides parent method to call
 coordinator's process_data method on bstream
-
 - class EngineConsumer:
 based of Consumer class from kafka package,
 consume method method overrides parent method to
 initialize + start a ProcessBstreamThread
 whenever message is consumed
-
 - class Engine:
 takes list of kafka topic strings,
 generate...
-
 """
 
 
 import json
-import ast
 from copy import deepcopy
 from time import time
 from threading import Thread
-from multiprocessing import Process, Pipe
+from multiprocessing import Process, Queue
 from strom.kafka.topics.checker import TopicChecker
 from strom.kafka.consumer.consumer import Consumer
 from strom.coordinator.coordinator import Coordinator
@@ -40,7 +34,7 @@ __version__ = "0.1"
 __author__ = "Molly <molly@tura.io>"
 
 
-class ProcessBStreamThread(Process):
+class ProcessBStreamThread(Thread):
     """
     Creates thread to call coordinator's process_data on batch of data from kafka
     """
@@ -50,41 +44,26 @@ class ProcessBStreamThread(Process):
         :param data: message passed from kafka consumer, in bytes
         """
         super().__init__()
-        self.coordinator = Coordinator()
-        logger.fatal("init process thread")
         self.data = data
+        self.coordinator = Coordinator()
 
     def run(self):
         stopwatch['processor timer {}'.format(self.name)].start()
         logger.debug("Starting processor thread")
         logger.debug(self.data[0])
+        # self.data = [json.loads(data) for data in self.data]
         self.coordinator.process_data_async(self.data, self.data[0]["stream_token"])
         stopwatch['processor_timer {}'.format(self.name)].stop()
         logger.debug("Terminating processor thread")
 
-class JSONLoader(Process):
-    def __init__(self, pipe):
-        super().__init__()
-        self.pipe_end = pipe
-        self.is_running = None
-
-    def run(self):
-        self.is_running = True
-        logger.fatal("running json loader")
-        while self.is_running:
-            piped = self.pipe_end.recv()
-            if piped is not None:
-                self.pipe_end.send(json.loads(piped))
 
 class EngineConsumer(Consumer):
     def __init__(self, url, topic, buffer, timeout=-1):
         super().__init__(url, topic, timeout=timeout)
         self.buffer = buffer
-        #self.json_loader = JSONLoader(self.buffer)
         self.topic = topic
         self.topic_name = topic.decode('utf-8')
         logger.info("Initializing EngineConsumer with timeout: {} ms".format(timeout))
-        logger.fatal("INITIALIZING consumer")
 
     def consume(self):
         self.consumer.start()  # auto-start
@@ -94,10 +73,9 @@ class EngineConsumer(Consumer):
                 stopwatch['{}_consumer_timer'.format(self.topic_name)].start()
                 # logger.fatal(msg.value.decode("utf-8"))
                 message = msg.value.decode("utf-8")
-                # message["engine_rules"]["kafka"]
-                self.buffer.append(message)
-                #logger.fatal("Message consumed: offset {}".format(msg.offset))
+                logger.debug("Message consumed: offset {}".format(msg.offset))
                 logger.debug("Message type: {}".format(type(message)))
+                self.buffer.append(message)
                 stopwatch['{}_consumer_timer'.format(self.topic_name)].stop()
             else:
                 logger.warning("Consumed empty message")
@@ -115,38 +93,52 @@ class ConsumerThread(Thread):
         logger.debug("Initializing Consumer Thread with timeout: {} ms".format(timeout))
 
     def run(self):
-        logger.fatal("CONSUMING ITS REAL")
         self.consumer_running = True
+        logger.debug("Starting consumer")
         self.consumer.consume()
         self.consumer_running = False
         if self.consumer_running is False:
-            self.consumer.json_loader.is_running = False
             self.consumer.stahp()
             logger.debug("Consumer terminated")
 
+class Processor(Process):
+    def __init__(self, queue):
+        super().__init__()
+        self.q = queue
+        self.coordinator = Coordinator()
+        self.is_running = None
+
+    def run(self):
+        self.is_running = True
+        logger.fatal("running json loader")
+        while self.is_running:
+            json_data = self.q.get()
+            data = [json.loads(datum) for datum in json_data]
+            self.coordinator.process_data_async(data[0], data[0][0]["stream_token"])
 
 class EngineThread(Thread):
     def __init__(self, url, topic, consumer_timeout=-1):
         super().__init__()
         self.buffer = []
-        self.url = 'localhost:9092'
+        self.url = url
         self.topic = topic
         self.topic_name = topic.decode('utf-8')
+        self.message_q = Queue()
+        self.number_of_processors = 8
+        self.processors = []
         self.buffer_record_limit = config["buffer_record_limit"]
         self.buffer_time_limit_s = config["buffer_time_limit_s"]
-        #self._init_sub_buffers()
-        #self.parent_consumer_pipe, self.child_consumer_pipe = Pipe()
-        self.parent_json_buffer_pipe, self.child_json_buffer_pipe = Pipe()
-        self.json_loader = JSONLoader(self.child_json_buffer_pipe)
+        #self.processor = Processor(self.child_json_buffer_pipe)
+        #self.receiver = ReceiverThread(self.parent_json_buffer_pipe)
         self.consumer_thread = ConsumerThread(self.url, self.topic, self.buffer, timeout=consumer_timeout)
         logger.info("Initializing Engine Thread for topic {} with Consumer timeout: {}".format(self.topic_name, consumer_timeout))
-        self.json_loader.start()
-        #logger.debug("Buffer limit params: {} records or {} seconds".format(config["buffer_record_limit"], config["buffer_time_limit_s"]))
+        self._init_processors()
+        #self.receiver.start()
 
-    # def _init_sub_buffers(self):
-    #     substreams = ['Parham']
-    #     for i in substreams:
-    #         self.buffer[i] = []
+    def _init_processors(self):
+        for n in range(self.number_of_processors):
+            processor = Processor(self.message_q)
+            processor.start()
 
     def _empty_buffer(self):
         self.buffer = []
@@ -166,27 +158,31 @@ class EngineThread(Thread):
 
         while self.consumer_thread.is_alive():
             # logger.debug("Consumer thread running")
-            logger.fatal("IM THE FUCKING ENGINE THREAD")
-            self.buffer = self.consumer_thread.consumer.buffer
+            # logger.debug("Checking buffer")
             while len(self.buffer) < int(self.buffer_record_limit) and time() - timer < int(self.buffer_time_limit_s):
                 pass
             if len(self.buffer):
-                logger.fatal("Buffer max reached, exiting inner loop")
+                logger.debug("Buffer max reached, exiting inner loop")
                 buffer_data = deepcopy(self.buffer)
                 self._empty_buffer()
-
-                self.parent_json_buffer_pipe.send(buffer_data)
+                self.message_q.put(buffer_data)
             else:
                 logger.warning("No records in buffer to process")
-            piped_back = self.parent_json_buffer_pipe.recv()
-            if piped_back is not None:
-                logger.fatal(piped_back)
-                processor = ProcessBStreamThread(piped_back)
-                processor.start()
-            # timer = time()
-            # result = self._check_consumer()
-            # logger.debug("Consumer running: {}".format(result))
+            result = self._check_consumer()
+            logger.debug("Consumer running: {}".format(result))
         logger.info("Terminating Engine Thread")
+
+class ReceiverThread(Thread):
+    def __init__(self, pipe):
+        super().__init__()
+        self.pipe = pipe
+
+    def run(self):
+        while True:
+            received = self.pipe.recv()
+            json.loads(received)
+            processor = ProcessBStreamThread(received)
+            processor.start()
 
 
 class Engine(object):
@@ -206,7 +202,6 @@ class Engine(object):
         topics = self.topic_buddy.list()
         self.topics = [k.decode('utf-8') for k,v in topics.items()]
         logger.info("Registered topics: {}".format(self.topics))
-
 
     def _add_topic(self, topic):
         self.topics.append(topic)
@@ -244,7 +239,7 @@ class Engine(object):
         #    self._listen_for_new_topics(keep_listening=keep_listening)
 
 def main():
-    topics = ['Parham']#, 'Molly', 'David', 'Justine', 'Adrian', 'Kody', 'Lucy', 'Lucky', 'Ricky', 'Allison']
+    topics = ['Parham']
     engine = Engine()
     engine.run_from_list(topics)
 
