47a35ad790f4f6f6ee2080f8c7b6edbabc43861c
tura-david <david@tura.io>
2018-01-02 12:17:30 -0800

    Starting docstrings

--- python/Strom/strom/transform/transform.py
+++ python/Strom/strom/transform/transform.py
@@ -1,6 +1,7 @@
 """Abstract class for our transform functions"""
 
 from abc import ABCMeta, abstractmethod
+
 from strom.utils.logger.logger import logger
 
 
@@ -12,19 +13,30 @@ class Transformer(object):
         self.params = {}
 
     def add_measure(self, measure_name, measure_data):
-        """Method to select the parameter[s] for transformation"""
+        """Method to select the parameter[s] for transformation
+        :param measure_name: name of measure
+        :type measure_name: str
+        :param measure_data: the data for measure_name
+        :type measure_data: dict
+        """
         self.data[measure_name] = measure_data
         logger.debug("added measure %s" % (measure_name))
 
     def load_measures(self, measure_dict):
-        """Load a dict of measures into transformation"""
+        """Load a dict of measures into transformation
+        :param measure_dict: dictionary containing multiple measures following BStream formatting
+        :type measure_dict: dict
+        """
         logger.debug("added measure dict")
         for key, value in measure_dict.items():
             self.add_measure(key, value)
 
     @abstractmethod
     def load_params(self, params):
-        """Method for setting the parameters of the transformation"""
+        """Method for setting the parameters of the transformation
+        :param params: all the parameters needed for the transformation
+        :type params: dict
+        """
         raise NotImplementedError("subclass must implement this abstract method.")
 
     @abstractmethod
