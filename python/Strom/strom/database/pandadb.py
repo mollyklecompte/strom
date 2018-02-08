2b9916f4f5a9bdbe25a45f609aa14773d8c860e9
Adrian Agnic <adrian@tura.io>
2018-02-07 23:19:12 -0800

    [#155007296] finish base class methods for now

--- python/Strom/strom/database/pandadb.py
+++ python/Strom/strom/database/pandadb.py
@@ -5,6 +5,7 @@ from abc import ABCMeta, abstractmethod
 __version__='0.0.1'
 __author__='Adrian Agnic'
 
+
 class PandaDB(metaclass=ABCMeta):
 
     def __init__(self, conn):
@@ -12,46 +13,82 @@ class PandaDB(metaclass=ABCMeta):
         super().__init__()
 
     @abstractmethod
-    def select(self, query, df=None):
-        """ standard sql parametrized select """
-        if not df:
-            pass
-        pass
+    def select(self, query, pars, table):
+        """ standard sql parametrized select, default select all w/ given table name
+        :type query: str
+        :param query: standard sql query with ? or '?' for params
+        :type pars: list, tuple
+        :param pars: values to be passed into query
+        :return: pandas
+        """
+        if not query:
+            stmnt = "SELECT * FROM {0}".format(table)
+            defres = pandas.read_sql(stmnt, self.conn)
+            return defres
+        res = pandas.read_sql(str(query), self.conn, params=pars)
+        return res
 
     @abstractmethod
-    def create(self, query, df=None):
-        """ standard sql parametrized create """
+    def create(self, query, pars, df, table):
+        """
+        :type query: str
+        :param query: standard sql query with ? or '?' for params
+        :type pars: list, tuple
+        :param pars: values to be passed into query
+        :type df: pandas
+        :param df: row(s), as a dataframe, to be created
+        :type table: str
+        :param table: name of table
+        """
         if not df:
-            pass
-        pass
+            pandas.read_sql(str(query), self.conn, params=pars)
+        dfres = self.select(table='yup')
+        dfcat = dfres.concat(df, ignore_index=True)
+        dfcat.to_sql(str(table), self.conn, if_exists="replace")
 
     @abstractmethod
-    def read(self, query, df=None):
-        """ standard sql parametrized read """
+    def update(self, query, pars, df, table):
+        """
+        :type query: str
+        :param query: standard sql query with ? or '?' for params
+        :type pars: list, tuple
+        :param pars: values to be passed into query
+        :type df: pandas
+        :param df: row, as a dataframe, to be updated
+        :type table: str
+        :param table: name of table
+        """
         if not df:
-            pass
-        pass
+            pandas.read_sql(str(query), self.conn, params=pars)
+        dfres = self.select(table='yup')
+        dfres.loc[df['index'], :] = df
+        dfres.to_sql(str(table), self.conn, if_exists="replace")
 
     @abstractmethod
-    def update(self, query, df=None):
-        """ standard sql parametrized update """
-        if not df:
-            pass
-        pass
+    def delete(self, query, pars):
+        """ standard sql parametrized delete
+        :type query: str
+        :param query: standard sql query with ? or '?' for params
+        :type pars: list, tuple
+        :param pars: values to be passed into query
+        :return: pandas
+        """
+        pandas.read_sql(str(query), self.conn, params=pars)
 
     @abstractmethod
-    def delete(self, query, df=None):
-        """ standard sql parametrized delete """
-        if not df:
-            pass
-        pass
+    def table(self, df, table):
+        """
+        :type df: pandas
+        :param df: dataframe to seed table with
+        :type table: str
+        :param table: name of table
+        """
+        df.to_sql(str(table), self.conn, if_exists="replace")
 
     @abstractmethod
     def connect(self):
-        """ open database connection """
         pass
 
     @abstractmethod
     def close(self):
-        """ close database connection """
-        pass
+        self.conn.close()
--- python/Strom/strom/database/sqlitedb.py
+++ python/Strom/strom/database/sqlitedb.py
@@ -1,30 +1,35 @@
 """ sub-class of pandadb, utilizes sqlite3 """
-from strom.database.pandadb import PandaDB
+import sqlite3 as sql
+from pandadb import PandaDB
 
 __version__='0.0.1'
 __author__='Adrian Agnic'
 
-class SqliteDb(PandaDB):
-    def __init__(self):
-        super().__init__()
 
-    def select(self):
-        super().select("SELECT * FROM test;")
+class SqliteDB(PandaDB):
 
-    def create(self):
-        super().create()
-
-    def read(self):
-        super().read()
-
-    def update(self):
-        super().update()
-
-    def delete(self):
-        super().delete()
+    def __init__(self, filename):
+        self.db = str(filename)
+        self.conn = None
+        super().__init__(self.conn)
 
     def connect(self):
-        super().connect()
+        self.conn = sql.connect(self.db)
 
     def close(self):
         super().close()
+
+    def select(self, query=None, pars=None, table=None):
+        return super().select(query, pars, table)
+
+    def create(self, query, pars=None, df=None, table=None):
+        super().create(query, pars, df, table)
+
+    def update(self, query, pars=None, df=None, table=None):
+        super().update(query, pars, df, table)
+
+    def delete(self, query, pars):
+        super().delete(query, pars)
+
+    def table(self, df, table):
+        super().table(df, table)
