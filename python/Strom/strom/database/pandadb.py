cf7d814e5f14382cd7d8dbbac7bbc6b3bfd1205b
Adrian Agnic <adrian@tura.io>
2018-02-08 23:05:40 -0800

    [#155043892] finish and local test base methods/impl

--- python/Strom/strom/database/pandadb.py
+++ python/Strom/strom/database/pandadb.py
@@ -1,5 +1,10 @@
-""" abstract database class w/ default basic pandas sql statements"""
+""" abstract database class w/ default basic pandas sql statements
+~ NOTES ~
+*   pandas.to_sql method creates columns 'index' and 'level_0' for backup reference to original indices
+*   contains static wrapper function around json.dumps if needed
+"""
 import pandas
+import json
 from abc import ABCMeta, abstractmethod
 
 __version__='0.0.1'
@@ -9,13 +14,26 @@ __author__='Adrian Agnic'
 class PandaDB(metaclass=ABCMeta):
 
     def __init__(self, conn):
+        """
+        :type conn: database connection object
+        :param conn: must assign this property in child class through connect() override
+        """
         self.conn = conn
         super().__init__()
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~BASIC FUNCTIONS
+    @abstractmethod
+    def connect(self):
+        """ must be overridden with specific engine connection method """
+        pass
+
+    @abstractmethod
+    def close(self):
+        self.conn.close()
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~BASIC METHODS
     @abstractmethod
     def select(self, query, pars, table):
-        """ standard sql parametrized select, default select all w/ given table name
+        """ standard sql parametrized select, default select all if just given table name
         :type query: str
         :param query: standard sql query with ? or '?' for params
         :type pars: list, tuple
@@ -32,18 +50,20 @@ class PandaDB(metaclass=ABCMeta):
         return res
 
     @abstractmethod
-    def table(self, df, table):
+    def table(self, df, table, action):
         """
         :type df: pandas
         :param df: dataframe to seed table with
         :type table: str
         :param table: name of table
+        :type action: str
+        :param action: action to take if table exists: 'replace', 'append', or 'fail'
         """
-        df.to_sql(str(table), self.conn, if_exists="replace")
+        df.to_sql(str(table), self.conn, if_exists=str(action))
 
     @abstractmethod
     def create(self, query, pars, df, table):
-        """
+        """ similar to table method but w/ query functionality and flexibility with # of columns
         :type query: str
         :param query: standard sql query with ? or '?' for params
         :type pars: list, tuple
@@ -62,15 +82,27 @@ class PandaDB(metaclass=ABCMeta):
             daf = daf.append(df)
             daf.to_sql(str(table), self.conn, if_exists="replace")
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~SPECIFIC METHODS
     @abstractmethod
-    def connect(self):
+    def table_exists(self, table):
+        """
+        :type table: str
+        :param table: name of table
+        """
+        try:
+            bewl = self.select(table=str(table))
+        except Exception as err:
+            print(err)
+            return False
+        return True
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~RETRIEVAL METHODS
+    @abstractmethod
+    def retrieve(self):
         pass
 
-    @abstractmethod
-    def close(self):
-        self.conn.close()
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~SPECIFIC FUNCTIONS
-    # @abstractmethod
-    def latest_version(self):
-        pass
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def serialize(stuff):
+    return json.dumps(stuff)
--- python/Strom/strom/database/sqlitedb.py
+++ python/Strom/strom/database/sqlitedb.py
@@ -1,8 +1,7 @@
 """ sub-class of pandadb, utilizes sqlite3 """
 import sqlite3 as sql
 import pickle
-import json
-from pandadb import PandaDB
+from pandadb import PandaDB, serialize
 
 __version__='0.0.1'
 __author__='Adrian Agnic'
@@ -22,18 +21,26 @@ class SqliteDB(PandaDB):
     def close(self):
         super().close()
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     def select(self, query=None, pars=None, table=None):
         return super().select(query, pars, table)
 
     def create(self, query=None, pars=None, df=None, table=None):
         super().create(query, pars, df, table)
 
-    def table(self, df, table):
-        super().table(df, table)
+    def table(self, df, table, action="replace"):
+        super().table(df, table, action)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    def table_exists(self, table):
+        return super().table_exists(table)
+
+    def retrieve(self):
+        pass
 
     def test(self):
         with open('dataframe.pkl', 'rb') as doc:
             df = pickle.load(doc)
-            df["location"] = df["location"].apply(lambda x: json.dumps(x))
-            df["not_location"] = df["not_location"].apply(lambda x: json.dumps(x))
+            df["location"] = df["location"].apply(lambda x: serialize(x))
+            df["not_location"] = df["not_location"].apply(lambda x: serialize(x))
             self.table(df=df, table='test')
