d32be0e617887105f08d0d3d37fbc0cb13da60bc
Adrian Agnic <adrian@tura.io>
2018-02-12 15:27:05 -0800

    change serialize to abstract

--- python/Strom/strom/database/sqlitedb.py
+++ python/Strom/strom/database/sqlitedb.py
@@ -1,7 +1,6 @@
 """ sub-class of pandadb, utilizes sqlite3 """
 import sqlite3 as sql
-import pickle
-from pandadb import PandaDB, serialize
+from pandadb import PandaDB
 
 __version__='0.0.1'
 __author__='Adrian Agnic'
@@ -12,11 +11,11 @@ class SqliteDB(PandaDB):
     def __init__(self, filename):
         self.db = str(filename)
         self.conn = None
+        super().__init__(self.conn)
 
-    def connect(self):
+    def open(self):
         """ pandadb override """
         self.conn = sql.connect(self.db)
-        super().__init__(self.conn)
 
     def close(self):
         super().close()
@@ -25,11 +24,11 @@ class SqliteDB(PandaDB):
     def select(self, query=None, pars=None, table=None):
         return super().select(query, pars, table)
 
-    def create(self, query=None, pars=None, df=None, table=None):
-        super().create(query, pars, df, table)
-
-    def table(self, df, table, action="replace"):
+    def table(self, df, table, action="fail"):
         super().table(df, table, action)
+
+    def create(self, df=None, table=None):
+        super().create(df, table)
 
     def query(self, stmnt):
         """ pandadb override, normal sqlite query method """
@@ -44,16 +43,5 @@ class SqliteDB(PandaDB):
     def retrieve(self):# NOTE TODO
         pass
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    def serialarrays(self, df):
-        """ handle unruly datatypes for conversion to sql table """
-        df["location"] = df["location"].apply(lambda x: serialize(x))
-        df["not_location"] = df["not_location"].apply(lambda x: serialize(x))
-        return df
-
-    def test(self):
-        with open('dataframe.pkl', 'rb') as doc:
-            df = pickle.load(doc)
-            df["location"] = df["location"].apply(lambda x: serialize(x))
-            df["not_location"] = df["not_location"].apply(lambda x: serialize(x))
-            self.table(df=df, table='test')
+    def serialize(self, df, fields):
+        return super().serialize(df, fields)
