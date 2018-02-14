b1af45035f4ec0e5f31eaa737dd49dd4f0400dc9
molly lecompte <molly@tura.io>
2018-02-14 15:23:24 -0800

    adds StorageInterface abstract class [#155208591]

---
+++ python/Strom/strom/storage/abstract_interface.py
@@ -0,0 +1,79 @@
+from abc import ABCMeta, abstractmethod
+
+
+__version__ = '0.0.1'
+__author__ = 'Molly LeCompte'
+
+
+class StorageInterface(metaclass=ABCMeta):
+
+    def __init__(self):
+        """
+
+        """
+        super().__init__()
+
+    @abstractmethod
+    def store_template(self, template):
+        """
+        Stores template
+        :param template: dstream template
+        :type template: dict
+        :return:
+        :rtype:
+        """
+        raise NotImplementedError("subclass must implement this abstract method.")
+
+    @abstractmethod
+    def retrieve_template_by_id(self, template_id, *args):
+        """
+        Retrieves template by id
+        :param template_id: the template id
+        :type template_id:
+        :return: dstream template
+        :rtype: dict
+        """
+        raise NotImplementedError("subclass must implement this abstract method.")
+
+    @abstractmethod
+    def retrieve_current_template(self, stream_token):
+        """
+        Retrieves current template for stream by stream token
+        :param stream_token: stream token
+        :type stream_token: str
+        :return:
+        :rtype:
+        """
+        raise NotImplementedError("subclass must implement this abstract method.")
+
+    @abstractmethod
+    def store_bstream(self, bstream):
+        """
+        Stores bstream
+        :param bstream: bstream dict
+        :type bstream: dict
+        :return:
+        :rtype:
+        """
+        raise NotImplementedError("subclass must implement this abstract method.")
+
+    @abstractmethod
+    def retrieve_data(self, stream_token, **retrieval_kwargs):
+        """
+        Retrieves raw data, optionally within certain parameters i.e. timestamp range
+        :param stream_token:
+        :type stream_token:
+        :param retrieval_kwargs: retrieval params i.e. raw=True, start_ts=45945, end_ts=None
+        :type retrieval_kwargs: dict
+        :return: result list
+        :rtype: list
+        """
+        raise NotImplementedError("subclass must implement this abstract method.")
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~NON ABSTRACT
+
+    def retrieve_all_current_templates(self, stream_tokens: list):
+        templates = [self.retrieve_current_template(token) for token in stream_tokens]
+
+        return templates
