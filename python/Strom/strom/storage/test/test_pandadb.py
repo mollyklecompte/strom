a78405e66b851d0e89aad3af98a30679b3969a0a
Adrian Agnic <adrian@tura.io>
2018-02-15 11:27:05 -0800

    [#155098018] finish create test

--- python/Strom/strom/storage/test/test_pandadb.py
+++ python/Strom/strom/storage/test/test_pandadb.py
@@ -13,7 +13,7 @@ class TestPandaDB(unittest.TestCase):
     def setUp(self):
         self.db = SqliteDB(":memory:")
         self.db.connect()
-        self.daf = pandas.DataFrame({"name":['aj', 'as', 'aj'], "version":[0.5, 1.5, 2.0]}, index=[0,1,2])# replace with actual data
+        self.daf = pandas.DataFrame({"name":['aj', 'as', 'aj'], "version":[0.5, 1.5, 2.0]}, index=[0,1,2])
 
     def test_table(self):
         self.db.table(df=self.daf, table='test', action='replace')
@@ -27,17 +27,15 @@ class TestPandaDB(unittest.TestCase):
         del res['index']
         self.assertIsNone(pandas.util.testing.assert_frame_equal(res, self.daf))
 
-    # def test_create(self):# NOTE
-    #     self.db.table(df=self.daf, table='test', action='replace')
-    #     newdaf = pandas.DataFrame({"name": "test", "version": 0.1, "extra":5}, index=[0])
-    #     self.db.create(df=newdaf, table='test')
-    #     res = self.db.select(table='test')
-    #     del res['index']
-    #     del res['level_0']
-    #     ores = self.daf.append(newdaf)
-    #     ores.reset_index()
-    #     res.reset_index()
-    #     self.assertIsNone(pandas.util.testing.assert_frame_equal(res, ores))
+    def test_create(self):
+        self.db.table(df=self.daf, table='test', action='replace')
+        odaf = self.db.select(table='test')
+        adddaf = pandas.DataFrame({"name":"aj", "version":0.6, "extra":5}, index=[0])
+        self.db.create(df=adddaf, table='test')
+        newdaf = self.db.select(table='test')
+        odaf = odaf.append(adddaf)
+        odaf = odaf.reset_index()
+        self.assertIsNone(pandas.util.testing.assert_frame_equal(odaf, newdaf))
 
     def test_exists(self):
         self.assertFalse(self.db.exists('test'))
