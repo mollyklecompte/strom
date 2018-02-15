eb968dee82f1ee8af6640f500d1ad0de16351b54
Adrian Agnic <adrian@tura.io>
2018-02-15 11:13:17 -0800

    [#155098018] sqlitedb testing minus one test

--- python/Strom/strom/storage/test/test_pandadb.py
+++ python/Strom/strom/storage/test/test_pandadb.py
@@ -1,6 +1,8 @@
 """ pandadb tests """
 import unittest
 import pandas
+import json
+from time import sleep
 from sqlitedb import SqliteDB
 
 __version__='0.0.1'
@@ -11,10 +13,58 @@ class TestPandaDB(unittest.TestCase):
     def setUp(self):
         self.db = SqliteDB(":memory:")
         self.db.connect()
-        self.daf = pandas.DataFrame({}, index=[])
+        self.daf = pandas.DataFrame({"name":['aj', 'as', 'aj'], "version":[0.5, 1.5, 2.0]}, index=[0,1,2])# replace with actual data
 
     def test_table(self):
-        pass
+        self.db.table(df=self.daf, table='test', action='replace')
+        res = self.db.select(query="SELECT * FROM test;")
+        del res['index']
+        self.assertIsNone(pandas.util.testing.assert_frame_equal(res, self.daf))
+
+    def test_select(self):
+        self.db.table(df=self.daf, table='test', action='replace')
+        res = self.db.select(table='test')
+        del res['index']
+        self.assertIsNone(pandas.util.testing.assert_frame_equal(res, self.daf))
+
+    # def test_create(self):# NOTE
+    #     self.db.table(df=self.daf, table='test', action='replace')
+    #     newdaf = pandas.DataFrame({"name": "test", "version": 0.1, "extra":5}, index=[0])
+    #     self.db.create(df=newdaf, table='test')
+    #     res = self.db.select(table='test')
+    #     del res['index']
+    #     del res['level_0']
+    #     ores = self.daf.append(newdaf)
+    #     ores.reset_index()
+    #     res.reset_index()
+    #     self.assertIsNone(pandas.util.testing.assert_frame_equal(res, ores))
+
+    def test_exists(self):
+        self.assertFalse(self.db.exists('test'))
+        self.db.table(df=self.daf, table='test', action='replace')
+        self.assertTrue(self.db.exists('test'))
+
+    def test_serialize(self):
+        serdaf = self.db.serialize(df=self.daf, fields=('name',))
+        try:
+            unser = json.loads(serdaf.at[0, 'name'])
+        except:
+            self.fail()
+
+    def test_retrieve(self):
+        self.db.table(df=self.daf, table='test', action='replace')
+        res = self.db.retrieve(table='test', col='name', val="'aj'", latest=True)
+        ores = self.db.select(query="SELECT * FROM test WHERE version=2.0;")
+        self.assertIsNone(pandas.util.testing.assert_frame_equal(res, ores))
+
+    def test_query(self):
+        self.db.table(df=self.daf, table='test', action='replace')
+        self.assertTrue(self.db.exists('test'))
+        self.db.query("DROP table test;")
+        self.assertFalse(self.db.exists('test'))
+
+    def tearDown(self):
+        self.db.close()
 
 
 if __name__ == '__main__':
