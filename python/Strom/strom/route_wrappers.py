adcf090c40fac5a2a7101fb2ca24c5e845a46cfe
Adrian Agnic <adrian.agnic@hotmail.com>
2018-02-23 17:12:00 -0800

    Delete route_wrappers.py

--- python/Strom/strom/route_wrappers.py
+++
@@ -1,7 +0,0 @@
-""" get/post wrapper functions for hitting server retrieval routes"""
-import requests
-
-
-def temp():
-    # example get request url
-    ret = requests.get("http://localhost:5000/api/retrieve/all?template_id=1")
