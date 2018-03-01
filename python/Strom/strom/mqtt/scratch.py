c255a456021d215bfa91b68f3a9b501b09b55cae
Adrian Agnic <adrian@tura.io>
2018-03-01 11:54:46 -0800

    flesh out some custom methods

--- python/Strom/strom/mqtt/scratch.py
+++ python/Strom/strom/mqtt/scratch.py
@@ -4,25 +4,33 @@ __version__ = "0.0.1"
 __author__ = "Adrian Agnic"
 
 
-class MQTTClient(mqtt.Client)
+class MQTTClient(mqtt.Client):
 
     def __init__(self):
         super().__init__(client_id="veryunique",
                         clean_session=False,
                         userdata=None,
-                        protocol=MQTTv311,
+                        protocol="MQTTv311",
                         transport="tcp")
 
-    def connect(self, remhost, remport, keepalive, binding):
-        super().connect(remhost, remport, keepalive, binding)
+    def connect(self, remhost, remport, keepalive, binding, async=False):
+        if async:
+            super().connect_async(host=remhost, port=remport, keepalive=keepalive, bind_address=binding)
+        super().connect(host=remhost, port=remport, keepalive=keepalive, bind_address=binding)
 
-    def disconnect(self):
-        super().disconnect()
+    def looper(self, timeout=1, start=False, stop=False, forever=False):
+        if start:
+            super().loop_start()
+        elif stop:
+            super().loop_stop()
+        elif forever:
+            super().loop_forever()
+        else:
+            super().loop(timeout=timeout)
 
-    def loop(self, timeout):
+    def send(self, topic, payload, qos, retain=False):
         pass
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~CALLBACKS
     def set_on_connect(self, func):
         super().on_connect = func
 
