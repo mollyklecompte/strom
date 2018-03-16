dfc73c89f792c17b4335a9e7b1da7ddf9556e144
Adrian Agnic <adrian@tura.io>
2018-03-16 13:54:37 -0700

    start run test

--- python/Strom/strom/mqtt/test/test_mqttclient.py
+++ python/Strom/strom/mqtt/test/test_mqttclient.py
@@ -1,25 +1,27 @@
 import unittest
+from time import sleep
 
-from .scratch import MQTTClient, publish, config
+from strom.mqtt.client import MQTTClient, publish, config
 
 
 class TestMQTTClient(unittest.TestCase):
 
     def setUp(self):
         self.c = 0
-        self.client = MQTTClient(f"testy{self.count}", asynch=True, **config)
+        self.client = MQTTClient(f"testy{self.c}", asynch=True, **config)
         self.c += 1
         self.client.on_message = self.on_message
         self.msg_arr = []
-        publish([], f"hello from testy{self.count-1}", **config)
+        publish([], f"hello from testy{self.c-1}", **config)
 
 
     def on_message(self, c, ud, msg):
-        self.msg_arr.append(msg)
-        return msg.payload
+        self.msg_arr.append(msg.payload)
 
     def test_run(self):
-        pass
+        self.client.run(**config)
+        sleep(2)
+        self.assertEqual(self.msg_arr[0], "hello from testy0")
 
 
     def tearDown(self):
