e3c1131e00d50ec9a5aa22ca88b9a4b47fe4d7e5
Adrian Agnic <adrian@tura.io>
2018-03-16 14:21:23 -0700

    run test passing

--- python/Strom/strom/mqtt/test/test_mqttclient.py
+++ python/Strom/strom/mqtt/test/test_mqttclient.py
@@ -1,26 +1,52 @@
 import unittest
 from time import sleep
 
-from strom.mqtt.client import MQTTClient, publish, config
+from strom.mqtt.client import MQTTClient, publish
+
+conf = {
+    "host": "iot.eclipse.org",
+    "port": 1883,
+    "keepalive": 60,
+    "timeout": 10,
+    "data": {
+        "topic": "psubatests",
+        "qos": 0,
+        "messages": [{
+            "topic": "psubatests",
+            "payload": "hello tura",
+            "qos": 0,
+            "retain": True
+        },
+        {
+            "topic": "psubatests",
+            "payload": "test123",
+            "qos": 0,
+            "retain": True
+        }
+        ]
+    }
+}
 
 
 class TestMQTTClient(unittest.TestCase):
 
     def setUp(self):
         self.c = 0
-        self.client = MQTTClient(f"testy{self.c}", asynch=True, **config)
+        self.client = MQTTClient(uid=f"testy{self.c}", userdata=conf, asynch=True, **conf)
+        publish([], f"hello from testy{self.c}", True, **conf)
         self.c += 1
         self.client.on_message = self.on_message
         self.msg_arr = []
-        publish([], f"hello from testy{self.c-1}", **config)
 
 
     def on_message(self, c, ud, msg):
-        self.msg_arr.append(msg.payload)
+        print(msg.payload)
+        self.msg_arr.append(msg.payload.decode())
 
     def test_run(self):
-        self.client.run(**config)
         sleep(2)
+        self.client.run(**conf)
+        sleep(5)
         self.assertEqual(self.msg_arr[0], "hello from testy0")
 
 
