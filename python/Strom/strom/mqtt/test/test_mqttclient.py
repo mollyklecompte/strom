0c9f52fed87cdbd2753a274ac1d21d6b845d5051
tura-david <david@tura.io>
2018-04-10 11:14:02 -0700

    fixed counter name

--- python/Strom/strom/mqtt/test/test_mqttclient.py
+++ python/Strom/strom/mqtt/test/test_mqttclient.py
@@ -1,15 +1,17 @@
 import unittest
-from .scratch import MQTTClient, publish, config
+
+from strom.mqtt.client import MQTTClient, publish, config
+
 
 class TestMQTTClient(unittest.TestCase):
 
     def setUp(self):
         self.c = 0
-        self.client = MQTTClient(f"testy{self.count}", asynch=True, **config)
+        self.client = MQTTClient(f"testy{self.c}", asynch=True, **config)
         self.c += 1
         self.client.on_message = self.on_message
         self.msg_arr = []
-        publish([], f"hello from testy{self.count-1}", **config)
+        publish([], f"hello from testy{self.c-1}", **config)
 
 
     def on_message(self, c, ud, msg):
